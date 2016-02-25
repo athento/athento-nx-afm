fc8daf9f7e71df6e15521fbdfb829b13e5eb4847
Paco Alías <pa@athento.com>
2016-02-25 13:59:23 +0100

    First commit from SVN repo

---
+++ README.txt
@@ -0,0 +1,99 @@
+
+                    Athento-Nuxeo Files Uploader
+
+What is it?
+-----------
+
+This plugin is used to upload files to a CMIS folder from a filesystem folder.
+
+How to use it?
+--------------
+
+In nuxeo.properties the following properties must be created:
+
+# Cuantas carpetas del FS estan siendo monitorizadas (N)
+AFM.NumberOfFolders=N
+
+#Para cada carpeta, ruta en el FS
+AFM.PathDocuments.1=/home/example/example_folder_1/ 
+AFM.PathDocuments.2=/home/example/example_folder_2/
+... 
+AFM.PathDocuments.N=/home/example/example_folder_N/ 
+
+#Para cada carpeta, ruta del workspace  (Sin el ultimo y colocando exactamente la misma ruta que se observa en la URL de Nuxeo dentro de ese WorkSpace)
+AFM.PathWorkspace.1=/default-domain/workspaces/work_example_1
+AFM.PathWorkspace.2=/default-domain/workspaces/work_example_2
+...
+AFM.PathWorkspace.N=/default-domain/workspaces/work_example_N
+
+#Para cada carpeta es necesario colocar el ID del workspace
+AFM.IdWorkspace.1=<docId>
+...
+
+# Si se define la propiedad, el tipo de documento indicado, será el que se cree, en caso contrario se creara un File.
+AFM.DocumentType = Type
+
+
+TODO
+----
+
+ * Refactor this component so that source and destiny folder are managed from data base.
+ * Refactor component so that "AFM.NumberOfFolders" property is not needed.
+ * Add a new configuration property to choose what order files should be read: alphabetical by name, creation date, size, extension, modification date
+ * Add a new configuration property for the queuing policy: FIFO, LIFO, random, etc.
+ * Add multithread support so that several files may be uploaded at once. Of course, keep control of conflicts, etc.
+ * Add a new configuration property to choose if files uploaded will be: removed, moved to another folder or renamed.
+ * Add checker to avoid blocking of this component (when there are files to upload, the component should upload them without any exception or blocking).
+ * Number of threads per monitored folder (1 by default).
+ * File extension (.* by default).
+ * Blocking Check: Max idle time per file (if a file waits fo more than X minutes/seconds, an alert will raise).
+ * Blocking Check Alert: WS URL or mail address to notify.
+
+
+Latest version
+--------------
+
+Details of the latest version can be found at http://doc.athento.com 
+
+Documentation
+-------------
+
+The documentation available can be found at http://doc.athento.com
+
+Instalation
+-----------
+
+Please see the file called INSTALL or http://doc.athento.com
+
+Licensing
+---------
+
+Please see the file called LICENSE
+
+Contact
+-------
+
+ * Ask for help at http://answers.athento.com
+
+ * You can contact anybody at http://www.athento.com/contact
+
+ * You can also write us to support@athento.com
+ 
+CHANGELOG
+---------
+
+Version 3.0:
+
+- Added listener GeneralEventListener to scheduler and listener.
+- Ingore sudo into lsof to load file.
+- Change package to org.athento
+-
+
+Version 2.3.1:
+
+Tarea: http://redmine.yerbabuena.es/issues/45005
+	- Added a new event: capturedDocument
+	- Fired "capturedDocument" when file is created into DM.
+	- Passed as parameter to the Event Context the path where the file was taken from
+	- Added default value "false" for property AFM.UpVersion
+	- Added a new dependency to AthentoNXUtils component (to manage events firing).
---
+++ src/main/java/org/athento/ecm/athento/afm/listeners/GeneralListener.java
@@ -0,0 +1,538 @@
+package org.athento.ecm.athento.afm.listeners;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.nuxeo.common.utils.IdUtils;
+import org.nuxeo.ecm.core.api.ClientException;
+import org.nuxeo.ecm.core.api.CoreSession;
+import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.DocumentRef;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.api.VersioningOption;
+import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
+import org.nuxeo.ecm.core.api.repository.Repository;
+import org.nuxeo.ecm.core.api.repository.RepositoryManager;
+import org.nuxeo.ecm.core.event.Event;
+import org.nuxeo.ecm.core.event.EventListener;
+import org.nuxeo.runtime.api.Framework;
+import org.nuxeo.runtime.transaction.TransactionHelper;
+import org.yerbabuena.athento.ecm.utils.AthentoNXUtils;
+
+/**
+ * General listener for AFM.
+ * 
+ * @contributor victorsanchez
+ * 
+ */
+public class GeneralListener implements EventListener {
+
+	private static final Log log = LogFactory.getLog(GeneralListener.class);
+
+	private static final String GENERAL_LISTENER = "generalServiceEvent";
+
+	private static final String CAPTURED_EVENT = "documentCaptured";
+
+	private static final String NUMBER_OF_FOLDERS = Framework
+			.getProperty("AFM.NumberOfFolders");
+
+	private static String OS = System.getProperty("os.name").toLowerCase();
+
+	private double totalDocuments;
+	private double totalLength;
+
+	// private static final String FILE_EXTENSION = ".pdf";
+
+	static boolean busy = false;
+
+	public void handleEvent(Event event) throws ClientException {
+		if (event.getName().equals(GENERAL_LISTENER)) {
+			afmRecursive();
+		}
+	}
+
+	private void afmRecursive() {
+
+		totalDocuments = 0;
+		totalLength = 0;
+
+		try {
+
+			int numberOfFolders = Integer.parseInt(NUMBER_OF_FOLDERS);
+			if (log.isDebugEnabled()) {
+				log.debug("AFM.NumberOfFolders: " + numberOfFolders);
+			}
+			for (int folderNumber = 1; folderNumber <= numberOfFolders; folderNumber++) {
+
+				String path = Framework.getProperty("AFM.PathDocuments."
+						+ folderNumber);
+				if (log.isDebugEnabled()) {
+					log.debug("AFM.PathDocuments." + folderNumber + ": " + path);
+				}
+				DocumentRef workspaceDestinyRef = null;
+				String idWorkspace = Framework.getProperty("AFM.IdWorkspace."
+						+ folderNumber);
+				if (idWorkspace == null) {
+					idWorkspace = Framework.getProperty("AFM.PathWorkspace."
+							+ folderNumber);
+					workspaceDestinyRef = new PathRef(idWorkspace);
+				} else {
+					workspaceDestinyRef = new IdRef(idWorkspace);
+				}
+				if (log.isDebugEnabled()) {
+					log.debug("AFM.IdWorkspace." + folderNumber + ": "
+							+ idWorkspace);
+				}
+				// Save start time of document upload
+				long uploadProccessStart = System.currentTimeMillis();
+
+				// Run in folder
+				folderIterator(path, workspaceDestinyRef);
+
+				// Save end time of document upload
+				long uploadProccessEnd = System.currentTimeMillis();
+
+				double uploadProccessTime = uploadProccessEnd
+						- uploadProccessStart;
+
+				uploadProccessTime = uploadProccessTime / 1000.0;
+
+				if (totalDocuments > 0) {
+
+					double averageTimePerDocument = uploadProccessTime
+							/ totalDocuments;
+					if (log.isDebugEnabled()) {
+						log.debug("\n\n===========================================================");
+						log.debug("Upload proccess finished. Elapsed time: "
+								+ uploadProccessTime + " secs to upload "
+								+ totalDocuments + " documents (" + totalLength
+								/ 1000000.0 + " MB).");
+						log.debug("Average upload time per document: "
+								+ averageTimePerDocument + " secs.");
+						log.debug("===========================================================\n\n");
+					}
+				}
+			}
+		} catch (NumberFormatException e) {
+			busy = false;
+			log.error(
+					"Error monitorizing folder... property \"AFM.NumberOfFolders\" is not a number",
+					e);
+		} catch (Exception e) {
+			busy = false;
+			log.error("Error monitorizing folder... ", e);
+		}
+	}
+
+	private void folderIterator(String path, DocumentRef parentFolderRef)
+			throws ClientException, IOException {
+		File directorio = new File(path);
+		String[] ficheros = directorio.list();
+		if (log.isDebugEnabled()) {
+			log.debug("Number of Documents in folder '" + path + "': "
+					+ ficheros.length);
+		}
+		CoreSession documentManager = getCoreSession("default");
+
+		// Parent folder document
+		DocumentModel parentFolder = documentManager
+				.getDocument(parentFolderRef);
+
+		// if(!busy){
+
+		busy = true;
+
+		for (int i = 0; i < ficheros.length; i++) {
+
+			File file = null;
+
+			if (isWindows()) {
+				file = new File(path + "\\" + ficheros[i]);
+			} else {
+				file = new File(path + "/" + ficheros[i]);
+			}
+
+			if (file.isDirectory()) {
+
+				// Save start time of document upload
+				long uploadDocumentStart = System.currentTimeMillis();
+
+				if (log.isInfoEnabled()) {
+					log.info("Uploading folder '" + file.getAbsolutePath()
+							+ "' to '" + parentFolder.getPathAsString() + "' ");
+				}
+				// File temporallocal = new
+				// File("/tmp/tmplocal"+System.currentTimeMillis()/*+FILE_EXTENSION*/);
+
+				String sufix = file.getName();
+
+				DocumentModel docModel = documentManager.createDocumentModel(
+						parentFolder.getPathAsString(),
+						IdUtils.generateStringId(), "Folder");
+
+				// Set title to document
+				docModel.setProperty("dublincore", "title", sufix);
+
+				// Set content to document
+				docModel.setProperty("file", "filename", sufix);
+
+				// Create document in repository
+				docModel = documentManager.createDocument(docModel);
+
+				// Fire captured event
+				// throwDocumentCapturedEvent(file, docModel);
+
+				// To increase the minor version of document
+				incrementMinorVersion(documentManager, docModel);
+
+				documentManager.save();
+
+				TransactionHelper.commitOrRollbackTransaction();
+				TransactionHelper.startTransaction();
+
+				folderIterator(file.getAbsolutePath(), docModel.getRef());
+
+				file.delete();
+
+				// Save end time of document upload
+				long uploadDocumentEnd = System.currentTimeMillis();
+
+				double estimatedTime = uploadDocumentEnd - uploadDocumentStart;
+
+				estimatedTime = estimatedTime / 1000.0;
+
+				if (log.isInfoEnabled()) {
+					log.info("Folder saved. Elapsed time: " + estimatedTime
+							+ " secs.");
+				}
+			} else {
+				if (documentIsOpen(file)) {
+					log.warn("The document is open... it will not be uploaded yet");
+				} else {
+
+					// Save start time of document upload
+					long uploadDocumentStart = System.currentTimeMillis();
+
+					FileBlob blob = new FileBlob(file);
+
+					totalLength += file.length();
+
+					// File temporallocal = new
+					// File("/tmp/tmplocal"+System.currentTimeMillis()/*+FILE_EXTENSION*/);
+
+					String sufix = file.getName();
+					File temporallocal = File.createTempFile("tmplocalAFM_",
+							sufix);
+
+					blob.transferTo(temporallocal);
+
+					FileBlob blob2 = new FileBlob(temporallocal);
+
+					if (log.isInfoEnabled()) {
+						log.info("Uploading document '"
+								+ file.getAbsolutePath() + "' to '"
+								+ parentFolder.getPathAsString() + "' ");
+					}
+					String doctype = Framework.getProperty("AFM.DocumentType",
+							"File");
+
+					DocumentModel docModel = documentManager
+							.createDocumentModel(
+									parentFolder.getPathAsString(),
+									IdUtils.generateStringId(), doctype);
+
+					// Set title to document
+					docModel.setProperty("dublincore", "title", sufix);
+
+					// Set content to document
+					docModel.setProperty("file", "filename", sufix);
+					docModel.setProperty("file", "content", blob2);
+
+					// Create document in repository
+					docModel = documentManager.createDocument(docModel);
+
+					// Fire captured event
+					// throwDocumentCapturedEvent(file, docModel);
+
+					// To increase the minor version of document
+					incrementMinorVersion(documentManager, docModel);
+
+					documentManager.save();
+
+					TransactionHelper.commitOrRollbackTransaction();
+					TransactionHelper.startTransaction();
+
+					file.delete();
+
+					temporallocal.delete();
+
+					// Save end time of document upload
+					long uploadDocumentEnd = System.currentTimeMillis();
+
+					double estimatedTime = uploadDocumentEnd
+							- uploadDocumentStart;
+
+					estimatedTime = estimatedTime / 1000.0;
+
+					if (log.isInfoEnabled()) {
+						log.info("Document saved. Elapsed time: "
+								+ estimatedTime + " secs.");
+					}
+				}
+			}
+
+			totalDocuments++;
+			// }
+
+			busy = false;
+		}
+	}
+
+	private void afmOneLevel() {
+		try {
+
+			int numberOfFolders = Integer.parseInt(NUMBER_OF_FOLDERS);
+			log.info("AFM.NumberOfFolders: " + numberOfFolders);
+
+			for (int folderNumber = 1; folderNumber <= numberOfFolders; folderNumber++) {
+
+				String path = Framework.getProperty("AFM.PathDocuments."
+						+ folderNumber);
+				log.info("AFM.PathDocuments." + folderNumber + ": " + path);
+
+				String pathWorkspace = Framework
+						.getProperty("AFM.PathWorkspace." + folderNumber);
+				log.info("AFM.PathWorkspace." + folderNumber + ": "
+						+ pathWorkspace);
+
+				File directorio = new File(path);
+				String[] ficheros = directorio.list();
+
+				log.info("Number of Documents in folder: " + ficheros.length);
+
+				CoreSession documentManager = getCoreSession("default");
+
+				if (!busy) {
+
+					busy = true;
+
+					// Save start time of document upload
+					long uploadProccessStart = System.currentTimeMillis();
+
+					for (int i = 0; i < ficheros.length; i++) {
+
+						File file = null;
+
+						if (isWindows()) {
+							file = new File(path + "\\" + ficheros[i]);
+						} else {
+							file = new File(path + "/" + ficheros[i]);
+						}
+
+						if (file.isDirectory()) {
+							continue;
+						}
+
+						if (documentIsOpen(file)) {
+							log.warn("The document is open... it will be not uploaded yet");
+						} else {
+
+							// Save start time of document upload
+							long uploadDocumentStart = System
+									.currentTimeMillis();
+
+							log.info("Uploading document '" + file.getName()
+									+ "' to '" + pathWorkspace + "' ");
+
+							FileBlob blob = new FileBlob(file);
+
+							// File temporallocal = new
+							// File("/tmp/tmplocal"+System.currentTimeMillis()/*+FILE_EXTENSION*/);
+
+							String sufix = file.getName();
+							File temporallocal = File.createTempFile(
+									"tmplocalAFM_", sufix);
+
+							blob.transferTo(temporallocal);
+
+							FileBlob blob2 = new FileBlob(temporallocal);
+
+							String type = Framework.getProperty(
+									"AFM.DocumentType", "File");
+
+							DocumentModel docModel = documentManager
+									.createDocumentModel(pathWorkspace,
+											IdUtils.generateStringId(), type);
+
+							// Set title to document
+							docModel.setProperty("dublincore", "title", sufix);
+
+							// Set content to document
+							docModel.setProperty("file", "filename", sufix);
+							docModel.setProperty("file", "content", blob2);
+
+							// Create document in repository
+							docModel = documentManager.createDocument(docModel);
+
+							// Fire captured event
+							throwDocumentCapturedEvent(file, docModel);
+
+							// To increase the minor version of document
+							incrementMinorVersion(documentManager, docModel);
+
+							documentManager.save();
+
+							TransactionHelper.commitOrRollbackTransaction();
+							TransactionHelper.startTransaction();
+
+							file.delete();
+
+							temporallocal.delete();
+
+							// Save end time of document upload
+							long uploadDocumentEnd = System.currentTimeMillis();
+
+							double estimatedTime = uploadDocumentEnd
+									- uploadDocumentStart;
+
+							estimatedTime = estimatedTime / 1000.0;
+
+							log.info("Document saved. Elapsed time: "
+									+ estimatedTime + " secs.");
+						}
+					}
+
+					busy = false;
+
+					// Save end time of document upload
+					long uploadProccessEnd = System.currentTimeMillis();
+
+					double uploadProccessTime = uploadProccessEnd
+							- uploadProccessStart;
+
+					uploadProccessTime = uploadProccessTime / 1000.0;
+
+					log.info("\n\n===========================================================");
+					log.info("Upload proccess finished. Elapsed time: "
+							+ uploadProccessTime + " secs.");
+					log.info("===========================================================\n\n");
+				}
+			}
+		} catch (NumberFormatException e) {
+			busy = false;
+			log.error(
+					"Error monitorizing folder... property \"AFM.NumberOfFolders\" is not a number",
+					e);
+		} catch (Exception e) {
+			busy = false;
+			log.error("Error monitorizing folder... ", e);
+		}
+	}
+
+	private void incrementMinorVersion(CoreSession documentManager,
+			DocumentModel docModel) throws ClientException {
+		String upVersion = Framework.getProperty("AFM.UpVersion", "false");
+		log.info("AFM.UpVersion." + upVersion);
+		if (upVersion.equals("true")) {
+			documentManager.checkIn(docModel.getRef(), VersioningOption.MINOR,
+					null);
+		}
+	}
+
+	private void throwDocumentCapturedEvent(File file, DocumentModel docModel) {
+		Map<String, Serializable> options = new HashMap<String, Serializable>();
+		options.put("filePath", file.getAbsolutePath());
+
+		try {
+			log.info("firing Event: " + CAPTURED_EVENT);
+			AthentoNXUtils.notifyEvent(CAPTURED_EVENT, docModel, options, null,
+					null, true, false);
+		} catch (Exception e) {
+			log.error("Error firing Event: ", e);
+		}
+	}
+
+	private boolean documentIsOpen(File document) {
+
+		String[] command = { "lsof", document.getAbsolutePath() };
+
+		try {
+			Process proc = Runtime.getRuntime().exec(command);
+
+			// CHECKING STANDARD OUTPUT
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(proc.getInputStream()));
+			String line;
+
+			line = bufferedReader.readLine();
+			if (line != null) {
+				log.info("Document is right now being used by:");
+				line = bufferedReader.readLine();
+				while (line != null) {
+					log.info("'" + line.split(" ")[0].toString() + "'");
+					line = bufferedReader.readLine();
+				}
+				return true;
+			}
+
+			// everything's OK
+			return false;
+		} catch (Exception e) {
+			log.error("EXCEPTION:");
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	public static boolean isWindows() {
+
+		return (OS.indexOf("win") >= 0);
+
+	}
+
+	public static boolean isMac() {
+
+		return (OS.indexOf("mac") >= 0);
+
+	}
+
+	public static boolean isUnix() {
+
+		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS
+				.indexOf("aix") > 0);
+
+	}
+
+	private static CoreSession getCoreSession(String repo)
+			throws ClientException {
+
+		CoreSession systemSession;
+		try {
+			Framework.login();
+			RepositoryManager manager = Framework
+					.getService(RepositoryManager.class);
+			Repository repository = manager.getRepository(repo);
+			if (repository == null) {
+				log.info("repository " + repo + " not in available repos: "
+						+ manager.getRepositories());
+				throw new ClientException("cannot get repository: " + repo);
+			}
+			systemSession = repository.open();
+		} catch (ClientException e) {
+			throw e;
+		} catch (Exception e) {
+			throw new ClientException(
+					"Failed to open core session to repository " + repo, e);
+		}
+		return systemSession;
+	}
+
+}
